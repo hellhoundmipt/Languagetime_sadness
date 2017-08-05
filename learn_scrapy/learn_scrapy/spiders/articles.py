940bf570110dc7308ab34a48989a0c79ea49e81a
hellhoundmipt <maxfelmosmartin@gmail.com>
2017-08-05 19:33:51 +0300

    Added articles crawling and following functionality

--- learn_scrapy/learn_scrapy/spiders/articles.py
+++ learn_scrapy/learn_scrapy/spiders/articles.py
@@ -62,7 +62,7 @@ class FullPageSpider(scrapy.Spider):
 
 class PdaSpider(scrapy.Spider):
     name = "pda"
-    start_urls = ["http://4pda.ru/page/1/"]
+    start_urls = ["http://4pda.ru"]
     allowed_domains = ["4pda.ru"]
 
 
@@ -71,6 +71,17 @@ class PdaSpider(scrapy.Spider):
         for a in response.xpath('//a[contains(@class, "nickname")]/text()'):
             yield users.append(a.extract())     #сохраняем имена юзеров, чтобы потом их удалить из комментов
 
+        # Сохраняем статьи
+        div_article = response.xpath("//div[contains(@class, 'content-box')]")
+        pn = div_article[0].xpath(".//p[contains(@style, 'text-align:justify') or contains(@style, 'text-align: justify')]")
+        pl = []
+        for p in pn:
+            pl.append(p.xpath("string()").extract_first())
+
+        article_text = ' '.join(pl)
+        yield {'article': article_text}
+
+        # Сохраняем комментарии
         comment_text = ''
         pattern = re.compile('\.?\s*\r?$')
         for p in response.xpath('//p[contains(@class, "content")]/text()'):
@@ -102,12 +113,12 @@ class PdaSpider(scrapy.Spider):
                 #делаем первую букву заглавной
                 if comment_text[0].islower():
                     comment_text = comment_text.replace(comment_text[0], comment_text[0].upper(), 1)
-                    
+
                 #вставляем пробел после последней точки
                 comment_text = comment_text + ' '
-                
+
                 #сохраняем сами комментарии
-                yield {'comment': comment_text[:-1]}  
+                yield {'comment': comment_text[:-1]}
                 comment_text = ''
 
 
@@ -122,6 +133,6 @@ class PdaSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        for page_num in range(1, 10):
+        for page_num in range(1, 30):
             new_page = "http://4pda.ru/page/" + str(page_num)
             yield scrapy.Request(new_page, callback=self.parse_page)
